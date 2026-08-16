4843c062b1cfcb88a90fab1b8cc20c88d90d50ab
MorfSet <128420500+MorfSet@users.noreply.github.com>
2026-08-16 15:45:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Giro Consegne - service worker (build 20) */
-const CACHE = 'giro-consegne-b20';
+/* Giro Consegne - service worker (build 21) */
+const CACHE = 'giro-consegne-b21';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
